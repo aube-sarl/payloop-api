7fd73d265c7dddc206dd008f6ffc640964e6c10d
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-02-28 11:00:19 +0200

    Create echange rates and users tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_141611) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_28_084518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "exchange_rates", force: :cascade do |t|
+    t.string "base_currency"
+    t.string "target_currency"
+    t.decimal "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name", null: false
