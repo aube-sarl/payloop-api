f01888c1eb1b5a1ecd375930073235c5ced080ea
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-01 17:53:35 +0200

    Generate rails authentication

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: {}
+    render json: { status: "success", data: { users: @users } }
   end
 end
