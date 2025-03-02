adf5821ee53b41053d838f1d4f672b5d5eba8e3e
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-02 16:00:20 +0200

    Add comments

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -8,8 +8,10 @@ class Api::V1::TransactionsController < ApplicationController
   # verify sender balance
   rescue @sender[:balance] < transaction_params[:amount_sent]
     render json: { status: "fail", error: { message: { EN: "You don't have enough money to perform this transaction", FR: "Vous n'avez pas assez d'argent pour effectuer cette transaction, veuillez recharger votre compte" } } }, status: :unprocessable_entity
+  # Perform transaction
   @transaction = Transaction.new(transaction_params)
     if @transaction.save
+      # Update account balances
       @sender.update(balance: @sender[:balance] - transaction_params[:amount_sent] - transaction_params[:fees])
       @receiver.update(balance: @receiver[:balance] + transaction_params[:amount_received])
       render json: { status: "success", data: { new_balance: @sender[:balance], transaction: @transaction } }
