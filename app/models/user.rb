d17e3a50e59e63a8f95bd7cd1cfd375d1c5fe693
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-04 17:25:03 +0200

    Reinitialize repository

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  validates :email, presence: true
-  validates :phone_number, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :middle_name, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,11 +7,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  namespace :api do
-    namespace :v1 do
-      resources :exchange_rates, only: [ :index, :create ]
-      patch "exchange_rates/update_one", to: "exchange_rates#update_one"
-      get "exchange_rates/show_one", to: "exchange_rates#show_one"
-    end
-  end
 end
