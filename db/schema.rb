e447158db501d5caa8667c27e3a7a6c186833502
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-04 17:44:23 +0200

    Create account table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_04_153340) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_04_154150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.decimal "balance"
+    t.string "currency"
+    t.integer "user_id"
+    t.string "linked_phone_number_network"
+    t.string "linked_phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -23,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_04_153340) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "accounts", "users"
 end
