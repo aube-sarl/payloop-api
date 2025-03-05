f02eb3f2448ae6c82ea02fdc9b98b1e5c7d8c115
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-05 07:51:06 +0200

    Add users and accounts

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :accounts
+      end
+    end
+  end
 end
