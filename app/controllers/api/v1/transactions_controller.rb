04f28d2d928c32588163a6c5d83fcd59d6f786f2
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-05 08:50:48 +0200

    Create transactions controller

---
+++ app/controllers/api/v1/transactions_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::TransactionsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+
+  def transaction_params
+  end
+
+  def find_transactions
+  end
+end
