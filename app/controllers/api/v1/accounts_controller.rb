f2c7e38b1c3447ccae6b12e2e5cdebb9dd5712a7
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-05 08:19:41 +0200

    Implement create account logic

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::AccountsController < ApplicationController
 
     render json: { status: "success", data: { accounts: @accounts } }
   end
+
+  def create
+    @account = Account.new(account_params)
+    if @account.save
+      render json: { status: "success", data: { account: @account } }, status: :created
+    else
+      render json: { status: "fail", error: { message: { EN: "Account not created", FR: "Compte non créé" } } }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:user_id, :currency, :linked_phone_number_network, :linked_phone_number)
+  end
 end
