7405c9fde95f1ffe2a275cf2cc7cd713782e9700
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-05 07:57:52 +0200

    Create index method in accounts controller

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::AccountsController < ApplicationController
+  def index
+    @accounts = Account.where({ user_id: params(:user_id) })
+
+    render json: { status: "success", data: { accounts: @accounts } }
+  end
 end
