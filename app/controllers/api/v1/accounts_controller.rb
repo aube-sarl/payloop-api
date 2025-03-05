bd7e695cbcefc202206a298c479f957171cd7d23
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-05 08:35:43 +0200

    Add find account method

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -31,4 +31,10 @@ class Api::V1::AccountsController < ApplicationController
   def account_params
     params.require(:account).permit(:user_id, :currency, :linked_phone_number_network, :linked_phone_number)
   end
+
+  def find_account
+    @account = Account.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { status: "fail", error: { message: { EN: "Account not found", FR: "Compte non trouvé." } } }
+  end
 end
