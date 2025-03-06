55f0644e40d45d09869321bff58984d11d339033
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-06 20:14:06 +0200

    Create update exchange rate method

--- app/controllers/api/v1/exchange_rates_controller.rb
+++ app/controllers/api/v1/exchange_rates_controller.rb
@@ -14,4 +14,18 @@ class Api::V1::ExchangeRatesController < ApplicationController
       render json: { status: "fail", error: { message: { FR: "Echec de la creation du taux d'echange", EN: "Couldn't create exchange rate" } } }, status: :unprocessable_entity
     end
   end
+
+  def update
+    if @exchange_rate.update(exchange_rate_params)
+      render json: { status: "success", data: { exchange_rate: @exchange_rate, message: { FR: "Taux d'echange mis a jour", EN: "Exchange rate update." } } }
+    else
+      render json: { status: "fail", error: { message: { FR: "Echec de la mise a jour du taux d'echange", EN: "Couldn't update exchange rate" } } }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def exchange_rate_params
+    params.require(:exchange_rate).permit(:base_currency_id, :target_currency_id, :rate)
+  end
 end
