321f09ea9f8e094fd92bf13dc2d1c727f1447521
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-03-08 15:00:46 +0200

    Validate account properties

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,20 @@ class Account < ApplicationRecord
   validates :linked_phone_number, presence: true
 
   belongs_to :user, foreign_key: :user_id
+
+  before_save :verify_if_currency_exists, :initialize_balance
+
+  private
+
+  def initialize_balance
+    self.balance = 0
+  end
+
+  def verify_if_currency_exists
+    self.currency = self.currency.upcase
+    unless Currency.exists?(code: self.currency)
+      errors.add(:currency, "does not exist")
+      throw(:abort)
+    end
+  end
 end
