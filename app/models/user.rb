48f8723e115faffe97ec6de07c431d39a4759917
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-10-30 17:47:28 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :firstname, :lastname, :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :middlename, presence: false, default: ""
 end
